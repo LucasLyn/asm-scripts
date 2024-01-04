b8155ae3dd7ab11909f409f9d488c1bc99281813
LucasLyn <lucaslyn1@gmail.com>
2024-01-04 19:16:21 +0100

    Updated comments to make them clearer

--- hello_world.s
+++ hello_world.s
@@ -2,11 +2,11 @@
 .intel_syntax noprefix
 
 _start:
-    # sys_write
-    mov rax, 1              # sys_write() value
-    mov rdi, 1              # unsigned int fd | stdout fd = 1
-    lea rsi, [hello_world]  # const char *buf
-    mov rdx, 13             # size_t count | size of string
+    # Call sys_write() with Hello World string, then terminate
+    mov rax, 1              # 1 -> sys_write()
+    mov rdi, 1              # unsigned int fd -> stdout fd is 1
+    lea rsi, [hello_world]  # const char *buf -> "Hello World!\n"
+    mov rdx, 13             # size_t count -> size of string is 13
     syscall
     jmp _terminate
     
@@ -17,5 +17,5 @@ hello_world:
 
 _terminate:
     mov rax, 60 # sys_exit() value
-    mov rdi, 1  # int error_code
+    mov rdi, 1 # int error_code
     syscall
